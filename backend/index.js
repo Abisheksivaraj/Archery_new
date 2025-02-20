e8360a369a992db5fc122ce4273faf94bb81c4e7
Abisheksivaraj <128159000+Abisheksivaraj@users.noreply.github.com>
2025-02-20 17:37:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // Configure CORS with specific origin and methods
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from this origin
+    origin: "https://archery-new-frontend.vercel.app", // Allow requests from this origin
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     credentials: true, // Allow credentials (if required)
