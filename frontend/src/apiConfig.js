9fdd11df8ba60b3008d70fcb3e7b9518977b371b
abisheksivaraj <abisheksivaraj2001.com>
2025-02-20 18:10:38 +0530

    add

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-export const API_URL = "https://archery-warehouse-backend.vercel.app/";
+export const API_URL = "http://localhost:5555";
 
 export const api = axios.create({
   baseURL: API_URL,
