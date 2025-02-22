84150eea9d160a08222143313c08d3db438203f0
abisheksivaraj <abisheksivaraj2001.com>
2025-02-22 19:34:47 +0530

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./backend/.env" });
 
-let URI = process.env.URI;
+let URI = process.env.MONGO_URI;
 console.log(URI);
 const connectDb = () => {
   return mongoose.connect(URI);
