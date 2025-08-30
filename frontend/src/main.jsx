8937c60271a3b70fd3077edf3a32f3380ae7561f
Abisheksivaraj <abisheksivaraj2001@gmail.com>
2025-08-30 16:50:34 +0530

    add

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./styles.css";
-import App from './App.jsx'
-import Sidebar from './Components/Sidebar.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import App from "./App.jsx";
+import Sidebar from "./Components/Sidebar.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
- 
+  </StrictMode>
+);
